0ad8d54d7ba5531aee79663a0e48fc1261b7914b
Y-O-R-O <dziugas.g99@gmail.com>
2022-08-10 12:07:57 +0300

    *small fix

--- RepositoryLayer/Databases/Cache/CacheFactory.cs
+++ RepositoryLayer/Databases/Cache/CacheFactory.cs
@@ -2,20 +2,19 @@ namespace RepositoryLayer.Databases.Cache;
 
 public class CacheFactory
 {
-    private readonly IServiceProvider serviceProvider;
+    private readonly IServiceProvider _serviceProvider;
 
     public CacheFactory(IServiceProvider serviceProvider)
     {
-        this.serviceProvider = serviceProvider;
+        _serviceProvider = serviceProvider;
     }
 
     public ICache GetCacheService(CachingService cachingService)
     {
         //TO DO: ElseIfs/Swichcases violates open closed principle.
-        
         if (cachingService.Equals(CachingService.InMemory))
-            return (ICache)serviceProvider.GetService(typeof(MemoryCache));
+            return (ICache)_serviceProvider.GetService(typeof(MemoryCache));
 
-        return (ICache)serviceProvider.GetService(typeof(DistributedCache));
+        return (ICache)_serviceProvider.GetService(typeof(DistributedCache));
     }
 }
