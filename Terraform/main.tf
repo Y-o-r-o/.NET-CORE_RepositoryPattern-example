803198aa4660c520a0351daa1a7fc073fafca67d
Y-O-R-O <dziugas.g99@gmail.com>
2022-08-16 15:36:56 +0300

    *small security fix.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,16 +6,11 @@ terraform {
     storage_account_name = "testinctfo9pooooz"
     container_name       = "core-tfstate"
     key                  = "terraform.tfstate"
-    access_key           = "pFfKZf6Plav2bVYB04N4DPJgHm3tLHyPdVWP9tOBTKRLDOpjer3fIEsIfju1fWmAVJ4j/7cJWvEE+ASttT4JaA=="
   }
 }
 
 # Configure the Azure provider
 provider "azurerm" {
   environment     = "public"
-  subscription_id = var.azure-subscription-id
-  client_id       = var.azure-client-id
-  client_secret   = var.azure-client-secret
-  tenant_id       = var.azure-tenant-id
   features {}
 }
